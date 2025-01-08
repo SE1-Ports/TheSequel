7c5936eac45a14d824f894fe9f4608a041b7aa6b
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2025-01-08 17:30:58 +0300

    Upload update 24-25 hotfix.

---
+++ Sources/ModelsMP/Enemies/SS3/SpiderBig/SpiderBig.h
@@ -0,0 +1,26 @@
+// Animation names
+#define SPIDERBIG_ANIM_DEFAULT 0
+#define SPIDERBIG_ANIM_HANGING 1
+#define SPIDERBIG_ANIM_CLIMBDOWN 2
+#define SPIDERBIG_ANIM_UNDERGROUND 3
+#define SPIDERBIG_ANIM_DIGOUT 4
+#define SPIDERBIG_ANIM_HATCHPOSE 5
+#define SPIDERBIG_ANIM_HATCH 6
+#define SPIDERBIG_ANIM_IDLE 7
+#define SPIDERBIG_ANIM_WALK 8
+#define SPIDERBIG_ANIM_FIRE 9
+#define SPIDERBIG_ANIM_MELEE 10
+#define SPIDERBIG_ANIM_WOUND 11
+#define SPIDERBIG_ANIM_DEATH 12
+
+// Color names
+
+// Patch names
+
+// Names of collision boxes
+#define SPIDERBIG_COLLISION_BOX_PART_NAME 0
+
+// Attaching position names
+#define SPIDERBIG_ATTACHMENT_ELECTRICITY 0
+
+// Sound names
